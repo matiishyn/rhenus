ba2ddfab22528c48eaad57c51a872757c6280e14
Ivan Matiishyn <matiyishyn.ivan@gmail.com>
2019-06-04 15:51:28 +0300

    custom server

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const next = require('next');
+
+// https://github.com/isaachinman/next-i18next/blob/master/examples/simple/server.js
+// const nextI18NextMiddleware = require('next-i18next/middleware');
+// const nextI18next = require('./i18n');
+
+const dev = process.env.NODE_ENV !== 'production';
+const port = process.env.PORT || 3000;
+const app = next({dev});
+const handle = app.getRequestHandler();
+
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+
+    // server.use(nextI18NextMiddleware(nextI18next));
+
+
+    // server.get('/p/:id', (req, res) => {
+    //   const actualPage = '/post';
+    //   const queryParams = { title: req.params.id };
+    //   app.render(req, res, actualPage, queryParams);
+    // });
+
+    server.get('*', (req, res) => {
+      return handle(req, res);
+    });
+
+    server.listen(port, err => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${port}`);
+    });
+  })
+  .catch(ex => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
