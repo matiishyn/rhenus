e790d5dcdbc91a7846785122bff2a2989d7e7be7
Vitaliy <tolik@gmail.com>
2019-06-07 14:18:08 +0300

    fixed footer

--- components/common/footer/index.js
+++ components/common/footer/index.js
@@ -12,43 +12,14 @@ export const Footer = () => {
   // ];
 
   return (
-    <header className="page-footer d-flex">
-      <div className="one" />
-      <div className="two" />
-
-      {/*<div className="container d-flex align-content-between content-footer">*/}
-      {/*<div>1</div>*/}
-      {/*<div>2</div>*/}
-      {/*<div>3</div>*/}
-      {/*</div>*/}
-
-      {/*<div className="footer-text">*/}
-      {/*<span>*/}
-      {/*Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid,*/}
-      {/*dicta, explicabo. Aliquam laudantium mollitia nihil quaerat.*/}
-      {/*Consequatur cumque, cupiditate dignissimos eligendi enim fugit ipsum,*/}
-      {/*libero officiis, perferendis quaerat quis veniam.*/}
-      {/*</span>*/}
-      {/*</div>*/}
-      {/*<div className="footer-contact">*/}
-      {/*<span>Any question?</span>*/}
-      {/*<p>*/}
-      {/*Call us directly at 013 - 123 44 56 or e-mail us at*/}
-      {/*hi@werkenbijrhenus.nl{' '}*/}
-      {/*</p>*/}
-      {/*</div>*/}
-      {/*<div>*/}
-      {/*<div className="logo">*/}
-      {/*<img src="/static/images/logo.svg" alt="logo" />*/}
-      {/*</div>*/}
-      {/*<ul className="list-group">*/}
-      {/*{listItems.map(item => (*/}
-      {/*<li className={cx('')} key={item.id}>*/}
-      {/*<a href={item.href}>{item.title}</a>*/}
-      {/*</li>*/}
-      {/*))}*/}
-      {/*</ul>*/}
-      {/*</div>*/}
-    </header>
+    <div className="page-footer">
+      <div className="container">
+        <div className="d-flex align-items-stretch footer-content">
+          <div className="flex-fill">1</div>
+          <div className="flex-fill">2</div>
+          <div className="flex-fill blue">3</div>
+        </div>
+      </div>
+    </div>
   );
 };
