ea89f9c1904a7f4eb3080bf5e81b9aa474487a7d
Vitaliy <tolik@gmail.com>
2019-06-11 23:23:03 +0300

    Fixed style footer tablet

--- components/common/footer/index.js
+++ components/common/footer/index.js
@@ -15,24 +15,26 @@ export const Footer = () => {
     <div className="page-footer">
       <div className="container">
         <div className="d-flex justify-content-between footer-content">
-          <div className="first">
-            <span>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-              Cupiditate dicta eos esse iusto laudantium quo, saepe tenetur.
-              Aliquid animi consequatur, exercitationem facere itaque laudantium
-              nemo obcaecati placeat totam vitae voluptates.
-            </span>
-          </div>
-          <div className="second">
-            <span className="title">Any questions?</span>
-            <br />
-            <span>
-              Call us directly at
-              <a href="tel:+0131234456">013 - 123 44 56</a> or
+          <div className="tablet-flex d-flex justify-content-between">
+            <div className="first">
+              <span>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Cupiditate dicta eos esse iusto laudantium quo, saepe tenetur.
+                Aliquid animi consequatur, exercitationem facere itaque
+                laudantium nemo obcaecati placeat totam vitae voluptates.
+              </span>
+            </div>
+            <div className="second">
+              <span className="title">Any questions?</span>
               <br />
-              e-mail us at
-              <a href="mailto:hi@werkenbijrhenus.nl">hi@werkenbijrhenus.nl</a>
-            </span>
+              <span>
+                Call us directly at
+                <a href="tel:+0131234456">013 - 123 44 56</a> or
+                <br />
+                e-mail us at
+                <a href="mailto:hi@werkenbijrhenus.nl">hi@werkenbijrhenus.nl</a>
+              </span>
+            </div>
           </div>
           <div className="blue">
             <img src="/static/images/logo.svg" alt="logo" />
