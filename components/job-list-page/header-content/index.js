b8f4ed2fef93cb109ca35c38ce918c4f7d9b9c8d
Vitaliy <tolik@gmail.com>
2019-06-13 15:46:01 +0300

    Fixed style (fonts, color)

--- components/job-list-page/header-content/index.js
+++ components/job-list-page/header-content/index.js
@@ -6,15 +6,18 @@ const HeaderContent = props => {
   return (
     <div className="bg-header">
       <div className="container d-flex flex-column">
-        <div className="d-flex justify-content-between">
+        <div className="d-flex justify-content-between top-line">
           <div className="find-job">
             <h2 className="title-job">Find your tomorrow-job at Rhenus</h2>
             <span>211 jobs available</span>
           </div>
-          <div className="today">
+          <div className="today d-flex flex-column">
             <h2>Today, I'm feeding 40 000 cats</h2>
-            <p>Tomorrow I need to do it</p>
-            <span>40.000 cats, how does that work? -></span>
+            <span className="two">Tomorrow I need to do it</span>
+            <span>
+              40.000 cats, how does that work?{' '}
+              <span className="ricon-arrow-right" />
+            </span>
           </div>
         </div>
         <TopFilters
