ecf5d934e73eb8e05ca02e118659ebd00e18a4c1
Vitaliy <tolik@gmail.com>
2019-06-25 13:19:04 +0300

    Add localization buttom button line components

--- components/job-item-page/bottom-button-line/index.js
+++ components/job-item-page/bottom-button-line/index.js
@@ -1,35 +1,37 @@
 import React from 'react';
 import './index.scss';
+import { withNamespaces } from '../../../services/i18n';
 
-export const BottomButtonLine = () => {
+export const BottomButtonLine = withNamespaces('common')(props => {
+  const { t } = props;
   return (
     <div className="bottom-line-bg">
       <div className="container d-flex justify-content-between">
         <div className="button-item-line">
           <a href="#">
             <span className="ricon-arrow-back" />
-            Back to search results
+            {t('siteWide.backToSearch')}
           </a>
         </div>
         <div className="button-item-line">
           <a href="#">
             <span className="ricon-save" />
-            Save to job list
+            {t('siteWide.savedJobList')}
           </a>
         </div>
         <div className="button-item-line">
           <a href="#">
             <span className="ricon-pdf" />
-            Download PDF
+            {t('siteWide.downloadPDF')}
           </a>
         </div>
         <div className="button-item-line">
           <a href="#">
             <span className="ricon-share" />
-            Share job with someone
+            {t('siteWide.shareJob')}
           </a>
         </div>
       </div>
     </div>
   );
-};
+});
