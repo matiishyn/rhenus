71b1d6b4a450e00a8f53e4f063d226210e155408
Vitaliy <tolik@gmail.com>
2019-06-30 18:06:45 +0300

    Add function to "Back to top"

--- components/job-list-page/back-to-top/index.js
+++ components/job-list-page/back-to-top/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './index.scss';
 import { withNamespaces } from '../../../services/i18n';
+import { animateScroll as scroll } from 'react-scroll';
 
-export const BackToTop = withNamespaces('common')(props => {
-  const { t } = props;
-  return (
-    <div className="back-to-top-sm">
-      <a href="#" className="back-to-top">
-        {t('siteWide.backToTop')}
-      </a>
-    </div>
-  );
-});
+export class BackToTop extends React.Component {
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+  // const { t } = props;
+  render() {
+    return (
+      <div className="back-to-top-sm">
+        <a href="#" className="back-to-top" onClick={this.scrollToTop}>
+          {/*{t('siteWide.backToTop')}*/}
+          Back to Top
+        </a>
+      </div>
+    );
+  }
+}
+
+export default withNamespaces('common')(BackToTop);
