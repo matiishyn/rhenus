4f2d1ba3e6021bc592386e99328ac2edcda479bc
Vitaliy <tolik@gmail.com>
2019-07-02 16:19:07 +0300

    Fixed star

--- components/common/saved-to-job-list/index.js
+++ components/common/saved-to-job-list/index.js
@@ -3,9 +3,16 @@ import { withNamespaces } from '../../../services/i18n';
 
 export const SavedToJobList = withNamespaces('common')(props => {
   const { t, handleAddJobItem, active } = props;
+  const Star = () => {
+    if (active === true) {
+      return <span className="ricon-save-active" />;
+    } else {
+      return <span className="ricon-save" />;
+    }
+  };
   return (
     <div className="d-flex">
-      <span className={`${active ? 'ricon-save-active' : 'ricon-save'}`} />
+      <Star />
       <span onClick={handleAddJobItem}>{t('siteWide.savedJobList')}</span>
     </div>
   );
