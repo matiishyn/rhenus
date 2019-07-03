d8a96357ff0291597d1da798b666af14ddcbadef
Vitaliy <tolik@gmail.com>
2019-07-03 14:23:01 +0300

    Add style for 'top-page-filter'

--- components/job-list-page/top-page-filter/index.js
+++ components/job-list-page/top-page-filter/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import './index.scss';
 
 export const TopPageFilter = props => {
   const { total } = props;
   return (
     <div className="d-flex flex-column top-page-filter">
       <div className="d-flex top-page-filter-counter">
-        <span>{total}</span>
-        <span>jobs found</span>
+        <span>{total} jobs found</span>
       </div>
-      <div className="d-flex">
-        <span>
-          <span className="ricon-filter-remove" />
-          asdasdas
-        </span>
-        <span>
-          <span className="ricon-filter-remove" />
-          asdfasdasq
-        </span>
+      <div className="d-flex top-page-filter-item">
+        <ul className="d-flex flex-row">
+          <li>
+            <span>
+              <span className="ricon-filter-remove" />
+              asdasdas
+            </span>
+          </li>
+          <li>
+            <span>
+              <span className="ricon-filter-remove" />
+              asdasdas
+            </span>
+          </li>
+        </ul>
       </div>
     </div>
   );
