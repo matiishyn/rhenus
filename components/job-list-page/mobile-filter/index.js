5f6bd78a8f983744ba1fd0d67e76ea03482678b7
Vitaliy <tolik@gmail.com>
2019-07-05 15:00:12 +0300

    Add style for scroll menu

--- components/job-list-page/mobile-filter/index.js
+++ components/job-list-page/mobile-filter/index.js
@@ -26,37 +26,39 @@ export default class MobileFilter extends React.Component {
     return (
       isVisible && (
         <div className="mobile-menu animated slideInDown faster">
-          <Collapse in={open}>
-            <div id="example-collapse-text">
-              <TopFilters
-                locationEntries={locationEntries}
-                fieldOfWorkEntries={fieldOfWorkEntries}
-                divisionEntries={divisionEntries}
+          <div className="container">
+            <Collapse in={open}>
+              <div id="example-collapse-text">
+                <TopFilters
+                  locationEntries={locationEntries}
+                  fieldOfWorkEntries={fieldOfWorkEntries}
+                  divisionEntries={divisionEntries}
+                  filter={filter}
+                  onSearch={onSearch}
+                />
+              </div>
+            </Collapse>
+            <div className="d-flex bottom-line-filter">
+              <Button
+                onClick={() => this.setState({ open: !open })}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+              >
+                Hide filter
+                <span
+                  className={open ? 'ricon-filter-open' : 'ricon-hamburger'}
+                />
+              </Button>
+              <TopPageFilter
+                {...{
+                  locationEntries,
+                  employmentEntries,
+                  fieldOfWorkEntries,
+                  divisionEntries
+                }}
                 filter={filter}
-                onSearch={onSearch}
               />
             </div>
-          </Collapse>
-          <div className="d-flex bottom-line-filter">
-            <Button
-              onClick={() => this.setState({ open: !open })}
-              aria-controls="example-collapse-text"
-              aria-expanded={open}
-            >
-              Hide filter
-              <span
-                className={open ? 'ricon-filter-open' : 'ricon-hamburger'}
-              />
-            </Button>
-            <TopPageFilter
-              {...{
-                locationEntries,
-                employmentEntries,
-                fieldOfWorkEntries,
-                divisionEntries
-              }}
-              filter={filter}
-            />
           </div>
         </div>
       )
