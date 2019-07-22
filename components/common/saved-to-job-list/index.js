f49b90d6a117f10fde38cd6b2236ffb431ccf579
Vitaliy <tolik@gmail.com>
2019-07-22 11:31:54 +0300

    Fixed addtojoblist

--- components/common/saved-to-job-list/index.js
+++ components/common/saved-to-job-list/index.js
@@ -10,10 +10,19 @@ export const SavedToJobList = withNamespaces('common')(props => {
       return <span className="ricon-save" />;
     }
   };
+
+  const Saved = () => {
+    if (active === true) {
+      return <span>{t('siteWide.savedJobList')}</span>;
+    } else {
+      return <span>{t('siteWide.addJobList')}</span>;
+    }
+  };
   return (
     <div className="d-flex" onClick={handleAddJobItem}>
       <Star />
-      <span>{t('siteWide.savedJobList')}</span>
+      {/*<span>{t('siteWide.savedJobList')}</span>*/}
+      <Saved />
     </div>
   );
 });
