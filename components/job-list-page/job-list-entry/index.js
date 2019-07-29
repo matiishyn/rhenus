d9f34e04f8c81e9a6acbe9600601cd92964eb645
Vitaliy <tolik@gmail.com>
2019-07-29 10:55:48 +0300

    Fixed Job item card

--- components/job-list-page/job-list-entry/index.js
+++ components/job-list-page/job-list-entry/index.js
@@ -24,43 +24,58 @@ export const JobListEntry = withNamespaces('common')(props => {
           </span>
         </div>
         <div className="blue" style={{ backgroundImage: `url(${photoUrl})` }}>
-          <div className="gradient d-flex justify-content-between">
-            <div className="blue-left">
-              <ul className="list-group">
-                <li>
-                  <span className="ricon-employment" />
-                  {jobEntry.fields.employment.fields.description}
-                </li>
-                <li>
-                  <span className="ricon-division" />
-                  {jobEntry.fields.division.fields.description}
-                </li>
-                <li>
-                  <span className="ricon-division" />
-                  {jobEntry.fields.location.fields.description}
-                </li>
-              </ul>
-              <div className="job-save-mobile d-md-none">
-                <SavedToJobList
-                  handleAddJobItem={() => handleAddJobItem(jobEntry)}
-                  active={isActive}
-                />
-              </div>
+          <div className="gradient">
+            <div className="md-card-content">
+              <Link href={`/job?id=${jobEntry.sys.id}`} prefetch>
+                <a>
+                  <h3>{jobEntry.fields.title}</h3>
+                </a>
+              </Link>
+
+              <h4>{jobEntry.fields.tagline}</h4>
+              <span>
+                <span className="ricon-label-important" />
+                {jobEntry.fields.fieldOfWork.fields.description}
+              </span>
             </div>
-            <div className="d-flex flex-column blue-right">
-              <div className="job-save-desc d-sm-none d-md-block">
-                <SavedToJobList
-                  handleAddJobItem={() => handleAddJobItem(jobEntry)}
-                  active={isActive}
-                />
+            <div className="d-flex justify-content-between">
+              <div className="blue-left">
+                <ul className="list-group">
+                  <li>
+                    <span className="ricon-employment" />
+                    {jobEntry.fields.employment.fields.description}
+                  </li>
+                  <li>
+                    <span className="ricon-division" />
+                    {jobEntry.fields.division.fields.description}
+                  </li>
+                  <li>
+                    <span className="ricon-division" />
+                    {jobEntry.fields.location.fields.description}
+                  </li>
+                </ul>
+                <div className="job-save-mobile d-md-none">
+                  <SavedToJobList
+                    handleAddJobItem={() => handleAddJobItem(jobEntry)}
+                    active={isActive}
+                  />
+                </div>
               </div>
-              <div className="learn-more d-flex justify-content-center">
-                <Link href={`/job?id=${jobEntry.sys.id}`} prefetch>
-                  <a title="Job detail">
-                    {t('siteWide.learnMore')}{' '}
-                    <span className="ricon-arrow-right" />
-                  </a>
-                </Link>
+              <div className="d-flex flex-column blue-right">
+                <div className="job-save-desc d-sm-none d-md-block">
+                  <SavedToJobList
+                    handleAddJobItem={() => handleAddJobItem(jobEntry)}
+                    active={isActive}
+                  />
+                </div>
+                <div className="learn-more d-flex justify-content-center">
+                  <Link href={`/job?id=${jobEntry.sys.id}`} prefetch>
+                    <a title="Job detail">
+                      {t('siteWide.learnMore')}{' '}
+                      <span className="ricon-arrow-right" />
+                    </a>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
